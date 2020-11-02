9c17837afa197747d4d135524f4d6ad3b62d79d0
Frank Septillion <frankseptillion@msn.cn>
2020-11-02 21:05:35 +0800

    Initial Commit

---
+++ CellularInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CellularInfo
+//
+//  Created by 王跃琨 on 2020/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
